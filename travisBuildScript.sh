f41d77ecc6723acf951e5f0d106149c131dd1ece
Dmitry Odintsov <odintsoff@gmail.com>
2018-07-06 00:02:33 -0400

    [#1] Update Pegasus and Unicorn submodules
    Limit parallel build on travis to 2 to avoid problems with Assimp eating all RAM

--- travisBuildScript.sh
+++ travisBuildScript.sh
@@ -6,6 +6,11 @@ cd $BUILD_DIR
 
 CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DSLEIPNIR_SHLIBDEPS=OFF"
 
+if [ -n "$COMPILER_FLAGS" ]; then
+    CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CXX_FLAGS=$COMPILER_FLAGS"
+fi
+
 cmake $CMAKE_FLAGS ..
 
-make -j${nproc} package
+# Unicorn's dependency Assimp eats too much RAM when built in parallel
+make -j2 package
